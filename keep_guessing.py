28f8583099b852dffa1b441e0e8575df81bf370f
Natalie Wong <natnatwong531@gmail.com>
2024-11-03 17:38:48 -0500

    Update keep_guessing.py

--- keep_guessing.py
+++ keep_guessing.py
@@ -1,1 +1,12 @@
+PIN = "5"
+
+print("I have chosen a number between 1 and 10. Try to guess it.")
+entry = input("Your guess: ")
+
+while entry != PIN:
+    print("That is incorrect. Guess again.")
+    entry = input("Your guess: ")
+else: 
+  print("That's right! You're a good guesser.")
+
 
