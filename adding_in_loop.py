e8d412218925ee90a5ee02e3b5e98d4d55832201
Natalie Wong <natnatwong531@gmail.com>
2024-11-12 10:54:54 -0500

    Create adding_in_loop.py

---
+++ adding_in_loop.py
@@ -0,0 +1,13 @@
+print("I will add up the numbers you give me. ")
+      
+total = 0
+while True:
+# Ask user for number
+    answer = input("Number: ")
+    if anwer = 0: 
+        print(f"The total is {total}.")
+    else: 
+# Add number to total
+      total += answer
+      print(f"The total so far is {total}")
+
